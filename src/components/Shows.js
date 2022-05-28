628a3ea00f729db1cfae63304c452680c633933d
Lilit <lilit.grigoryan@accenture.com>
2022-05-28 22:34:18 +0300

    grid structure

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -5,7 +5,8 @@ import {getSeasons} from "../api/DataService.js";
 import Seasons from "./Seasons";
 // import ReactTable from '@kavience/react-table';
 // import MaterialTable from "material-table";
-
+import Grid from 'material-grid/dist/Grid/Grid';
+import Cell from 'material-grid/dist/Grid/Cell'
 
 function Shows(props) {
 
@@ -60,80 +61,91 @@ function Shows(props) {
     
     return (
         <> 
-         <ReactTable
-         
-            data={props.records}
-            columns={[
-            {
-                Header: "Tv Show Image",
-                accessor: "show.image.original",
-                minWidth: 100,
-                Cell: (row) => {
-                   return <div> {row.original.show.image? <img height={34} src={row.original.show.image.original } />: "" } </div>
-                    // return <div> <img height={34} src={row.original.show.image?row.original.show.image.original: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" } /> </div>
-                 },
-            },
-            {
-                Header: "Title",
-                // accessor: `${show.name? show.name:""}`,
-                // accessor: "show.name? show.name:",
-                // accessor: "name? name: ",
-                accessor: "show.name",
-                minWidth: 100
-            },
-            {
-                Header: "Description",
-                // accessor: `${show.summary? show.summary:""}`,
-                // accessor: "show.summary? show.summary:",
-                accessor: "show.summary",
-                minWidth: 300
-            },
-            {
-                Header: "End Date",
-                accessor: "show.ended",
-                minWidth: 100
-            },
-            {
-                Header: "Type",
-                accessor: "show.type",
-                minWidth: 100
-            },
-            {
-                Header: "Language",
-                accessor: "show.language",
-                minWidth: 100
-            },
-            ]}
-            getTrProps={(selected, rowInfo) => {
-                if (rowInfo && rowInfo.row) {
-                  return {
-                    onClick: (e) => {
-                        console.log("rowInfo",rowInfo);
-                        console.log("rowInfo row",rowInfo.row);
-                        console.log("row index ",rowInfo.index);
-                        // console.log("selected 0", selectedIn);
-                        setSelectedIn(rowInfo.index);
-                        console.log("selected 1", selectedIn);
-                    //   this.setState({
-                    //     selected: rowInfo.index
-                    //   })
+        <Grid>
+            <Cell col={6}>
+                <p> Shows </p>
+              <ReactTable
+                
+                data={props.records}
+                columns={[
+                {
+                    Header: "Tv Show Image",
+                    accessor: "show.image.original",
+                    minWidth: 100,
+                    Cell: (row) => {
+                        return <div> {row.original.show.image? <img height={34} src={row.original.show.image.original } />: "" } </div>
+                        // return <div> <img height={34} src={row.original.show.image?row.original.show.image.original: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" } /> </div>
                     },
-                    
-                    style: {
-                      background: rowInfo.index === selectedIn ? 'chocolate' : 'papayawhip',
-                      color: rowInfo.index === selectedIn ? 'white' : 'black'
+                },
+                {
+                    Header: "Title",
+                    // accessor: `${show.name? show.name:""}`,
+                    // accessor: "show.name? show.name:",
+                    // accessor: "name? name: ",
+                    accessor: "show.name",
+                    minWidth: 100
+                },
+                {
+                    Header: "Description",
+                    // accessor: `${show.summary? show.summary:""}`,
+                    // accessor: "show.summary? show.summary:",
+                    accessor: "show.summary",
+                    minWidth: 300
+                },
+                {
+                    Header: "End Date",
+                    accessor: "show.ended",
+                    minWidth: 100
+                },
+                {
+                    Header: "Type",
+                    accessor: "show.type",
+                    minWidth: 100
+                },
+                {
+                    Header: "Language",
+                    accessor: "show.language",
+                    minWidth: 100
+                },
+                ]}
+                getTrProps={(selected, rowInfo) => {
+                    if (rowInfo && rowInfo.row) {
+                    return {
+                        onClick: (e) => {
+                            console.log("rowInfo",rowInfo);
+                            console.log("rowInfo row",rowInfo.row);
+                            console.log("row index ",rowInfo.index);
+                            // console.log("selected 0", selectedIn);
+                            setSelectedIn(rowInfo.index);
+                            console.log("selected 1", selectedIn);
+                        //   this.setState({
+                        //     selected: rowInfo.index
+                        //   })
+                        },
+                        
+                        style: {
+                        background: rowInfo.index === selectedIn ? 'chocolate' : 'papayawhip',
+                        color: rowInfo.index === selectedIn ? 'white' : 'black'
+                        }
                     }
-                  }
-                  
-                }else{
-                  return {}
-                }
-              }}
-        />
-        {(seasons!==undefined || seasons!==null) ? <Seasons seasons={seasons}>
+                    
+                    }else{
+                    return {}
+                    }
+                }}
+            />
+            </Cell>
+            <Cell col={3}>
+                seasons
+                {(seasons!==undefined || seasons!==null) ? <Seasons seasons={seasons}>
 
-        </Seasons>: ""}
-       
+                 </Seasons>: ""}
+            </Cell>
+            <Cell col={3}>
+                 episodes
+            </Cell>
+        </Grid>
+                
      
         </>
     )
