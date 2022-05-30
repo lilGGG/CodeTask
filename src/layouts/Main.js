37f9688996756bc075ff6a5912bd7f03c8477cd7
Lilit <lilit.grigoryan@accenture.com>
2022-05-30 04:52:41 +0300

    routeusing

---
+++ src/layouts/Main.js
@@ -0,0 +1,16 @@
+// import Navbar from '../components/navbar/navbar'
+import  { BrowserRouter, Route, Routes } from "react-router-dom";
+import  Home from "../pages/Home";
+function Main() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default Main;
